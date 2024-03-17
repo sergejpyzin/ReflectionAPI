57210f73f6b29d1060deba1127a4977ffadb4042
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-03-17 11:56:05 +0300

    create methods AfterAll, AfterEach, BeforeAll and BeforeEach

---
+++ src/main/java/ru/sergej/BeforeEach.java
@@ -0,0 +1,11 @@
+package ru.sergej;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+public @interface BeforeEach {
+}
